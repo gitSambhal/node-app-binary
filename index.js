2b5e7e52e9466f9f7986c29ae09185b5e6d7a378
Suhail Akhtar <suhail.akhtar@tabsquare.com>
2023-02-17 20:44:28 +0530

    Added flag

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+process.env.NODE_SKIP_PLATFORM_CHECK = 1
 const { myLibFun } = require('./mylib')
 
-console.log('I will be exe')
+console.log('I will be exe updated', new Date().toString())
 myLibFun()
