583e21796a5189ea299bd812b62d5662a301b761
Suhail Akhtar <suhail.akhtar@tabsquare.com>
2023-02-21 23:34:03 +0530

    Added healthcheck and datadog logs

--- updater.js
+++ updater.js
@@ -4,6 +4,24 @@ const path = require('path')
 const dotenv = require('dotenv');
 const { cleanEnv, str, url, } = require('envalid');
 
+const DD_INFO = {
+  API_URL: 'https://http-intake.logs.datadoghq.eu/v1/input',
+  HOST: 'local',
+  SERVICE_NAME: 'CUSCSPI-AGENT-UPDATER',
+  SOURCE: 'nodejs',
+  LOG_LEVEL: {
+    ERROR: 'error',
+    LOG: 'log',
+    WARN: 'warn',
+  },
+}
+
+const PING_TYPES = {
+  SUCCESS: 'success',
+  FAIL: 'fail',
+  LOG: 'log',
+}
+
 const FILE_NAMES = {
   ENV: 'configuration.txt',
   VERSION: 'version.txt',
@@ -27,6 +45,9 @@ const env = cleanEnv(process.env, {
   JFROG_URL_ARTIFACT_FOLDER: url(),
   URL_API_TO_CHECK_VERSION: url(),
   JFROG_TOKEN: str(),
+  DD_API_KEY: str(),
+  TS_MERCHANT_KEY: str(),
+  HC_PING_URL_UPDATER: url(),
 });
 
 const filePathPatternInJfrog = `${env.JFROG_URL_ARTIFACT_FOLDER}/agent-win-${versionPattern}`
@@ -60,24 +81,32 @@ const getFilePathToSaveDownloadedAgent = (version) => {
   return filePath
 }
 
-const saveBinaryFile = async (version) => {
-  try {
-    const filePath = filePathPatternInJfrog.replace(versionPattern, version);
-    console.log(`Downloading the ${version} version`)
-    const res = await axios.get(filePath, {
-      responseType: 'arraybuffer',
-      headers
-    });
-    await fs.writeFile(getFilePathToSaveDownloadedAgent(version), res.data);
-    console.log('Updating the version info in the file')
-    await fs.writeFile(versionFilePath, String(version));
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const errorResp = error.response.data.toString()
-      console.log('Unable to download the file ', errorResp)
+const saveBinaryFile = (version) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const filePath = filePathPatternInJfrog.replace(versionPattern, version);
+      logMessage(`Downloading the version: ${version}`)
+      const res = await axios.get(filePath, {
+        responseType: 'arraybuffer',
+        headers
+      });
+      await fs.writeFile(getFilePathToSaveDownloadedAgent(version), res.data);
+      logMessage(`Version ${version} downloaded successfully, Updating the version info in the file`)
+      await fs.writeFile(versionFilePath, String(version));
+      return resolve()
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log('🚀 ~ file: updater.js:76 ~ saveBinaryFile ~ error', error);
+        let errorResp = error.message;
+        if (error.response && error.response.data) {
+          errorResp = error.response.data.toString()
+        }
+        logMessage('Unable to download the file ' + errorResp)
+      }
+      logError(error.message, error)
+      return reject(error.message)
     }
-    console.error(error.message)
-  }
+  })
 }
 
 const getTheLatestVersionInfoInJfrog = async () => {
@@ -87,50 +116,113 @@ const getTheLatestVersionInfoInJfrog = async () => {
     const res = await axios.get(filePath + '?properties', { headers })
     const version = res?.data?.properties?.version?.[0]
     if (!version) throw error
-    console.log('Latest version in jfrog: ', version);
+    logMessage('Latest version in jfrog: ' + version);
     return version
   } catch (error) {
-    console.error(error.message)
+    logError(error.message, error)
     throw error
   }
 }
 
-const getCurrentInstalledVersionNumber = async () => {
-  const error = new Error('Version info not found in version.txt file')
-  try {
-    const version = await (await fs.readFile(versionFilePath)).toString()
-    if (!version) throw error;
-    console.log('Current installed version: ', version)
-    return version;
-  } catch (error) {
-    console.error(error.message)
-    throw error;
-  }
+const getCurrentInstalledVersionNumber = () => {
+  return new Promise(async (resolve, reject) => {
+    const error = new Error('Version info not found in version.txt file')
+    try {
+      const version = await (await fs.readFile(versionFilePath)).toString()
+      if (!version) throw error;
+      logMessage('Current installed version: ' + version)
+      return resolve(version);
+    } catch (error) {
+      logError(error.message, error)
+      reject(error.message);
+    }
+  })
 }
 
 const getMerchantKey = async () => {
-  return 'HQ-123-456'
+  return env.TS_MERCHANT_KEY;
 }
 
-const checkApiToGetVersionToInstallForThisMerchant = async () => {
-  let versionToInstall;
-  try {
-    const currentMerchantKey = await getMerchantKey()
-    const { data: apiResp } = await axios.get(apiUrlToCheckTheAgentVersion)
-    versionToInstall = apiResp[currentMerchantKey];
-  } catch (error) {
-    console.error('Error occurred while checking the version from api or merchant key not found')
-  }
+const checkApiToGetVersionToInstallForThisMerchant = () => {
+  return new Promise(async (resolve, reject) => {
+    let versionToInstall;
+    try {
+      const currentMerchantKey = await getMerchantKey()
+      const { data: apiResp } = await axios.get(apiUrlToCheckTheAgentVersion)
+      versionToInstall = apiResp[currentMerchantKey];
+    } catch (error) {
+      logError('Error occurred while checking the version from api or merchant key not found')
+      return reject(error)
+    }
 
-  if (!versionToInstall) {
-    console.error('Version not found in the api for this merchant')
-    return;
-  }
-  const currentInstalledVersion = await getCurrentInstalledVersionNumber()
-  if (currentInstalledVersion == versionToInstall) {
-    console.warn('Correct version is installed already')
-    return;
-  }
-  saveBinaryFile(versionToInstall)
+    if (!versionToInstall) {
+      const msg = 'Version not found in the api for this merchant'
+      logError(msg)
+      return reject(msg)
+    }
+    const currentInstalledVersion = await getCurrentInstalledVersionNumber()
+    if (currentInstalledVersion == versionToInstall) {
+      const msg = 'Correct version is installed already'
+      logMessage(msg)
+      return reject(msg)
+    }
+    return resolve(saveBinaryFile(versionToInstall))
+  })
+
 }
-checkApiToGetVersionToInstallForThisMerchant();
+
+const main = () => {
+  logMessage('--------------- Start ---------------')
+  checkApiToGetVersionToInstallForThisMerchant()
+    .then(() => {
+      pingToHealthCheck({ type: PING_TYPES.SUCCESS })
+    })
+    .catch((error) => {
+      pingToHealthCheck({ type: PING_TYPES.FAIL, data: error })
+    }).finally(() => [
+      logMessage('--------------- End ---------------')
+    ])
+}
+main();
+
+// Types : success, fail, log
+function pingToHealthCheck({ type = PING_TYPES.SUCCESS, data = null } = {}) {
+  const typesMap = {
+    success: '',
+    fail: '/fail',
+    log: '/log',
+  }
+  const url = env.HC_PING_URL_UPDATER + typesMap[type];
+  return axios.post(url, data)
+}
+
+function logMessage(message = '') {
+  if (!message) return;
+  logToDataDog({ message, level: DD_INFO.LOG_LEVEL.LOG })
+}
+
+function logError(message = '', error = null) {
+  if (!message) return;
+  logToDataDog({
+    message, level: DD_INFO.LOG_LEVEL.ERROR, error
+  })
+}
+
+function logToDataDog({ message, level, error = null }) {
+  let headers = {
+    'Content-Type': 'application/json',
+    'DD-API-KEY': env.DD_API_KEY,
+  }
+  let payload = {
+    date: new Date().toISOString(),
+    ddsource: DD_INFO.SOURCE,
+    hostname: DD_INFO.HOST,
+    level,
+    message,
+    service: DD_INFO.SERVICE_NAME,
+    ...error && { error },
+  };
+  return axios.post(DD_INFO.API_URL, payload, {
+    headers: headers
+  });
+}
