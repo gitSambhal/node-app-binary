8d08962f15b93b0d59486836e341f8bc0cc5878d
Suhail Akhtar <suhail.akhtar@tabsquare.com>
2023-02-22 17:42:03 +0530

    Added getEnvFromDopplerAPI

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const axios = require('axios');
+
+const getEnvFromDopplerAPI = async () => {
+    const DOPPLER_TOKEN = 'dp.st.dev.8tdQjtTTORLAczirJEdio2TdJPrzR9iyod2DOw7h7LR'
+    const token = DOPPLER_TOKEN
+    const response = await axios.get(`https://${token}@api.doppler.com/v3/configs/config/secrets/download?format=json`)
+    const resp = response.data
+    console.log('🚀 ~ file: index.js:10 ~ getEnv ~ respx', resp);
+}
+
 process.env.NODE_SKIP_PLATFORM_CHECK = 1
 const { myLibFun } = require('./mylib')
 
 console.log('I will be exe updated', new Date().toString())
-myLibFun()
+console.log('Process.env ', { env: process.env })
+getEnvFromDopplerAPI()
