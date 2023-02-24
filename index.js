722a3d728a81e66b70889867be9ef32a8a532fe4
Suhail Akhtar <suhail.akhtar@tabsquare.com>
2023-02-24 10:48:34 +0530

    Added GCP Secret manager api to get the .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
 const { cleanEnv, str, } = require('envalid');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
 dotenv.config({ path: 'configuration.txt' });
 const env = cleanEnv(process.env, {
@@ -13,6 +14,22 @@ const getEnvFromDopplerAPI = async () => {
     const response = await axios.get(`https://${token}@api.doppler.com/v3/configs/config/secrets/download?format=json`)
     const resp = response.data
     console.log('🚀 ~ file: index.js:10 ~ getEnv ~ respx', resp);
+}
+
+async function getEnvFromGoogleSecretManager() {
+    const client = new SecretManagerServiceClient({
+        // Set the credentials property to your API token
+        credentials: {
+            private_key: 'your-private-key',
+            client_email: 'your-client-email',
+        },
+    });
+
+    const name = 'projects/YOUR_PROJECT_ID/secrets/myapp-env/versions/latest';
+    const [version] = await client.accessSecretVersion({ name });
+    const payload = version.payload.data.toString();
+    const env = JSON.parse(payload);
+    return env;
 }
 
 process.env.NODE_SKIP_PLATFORM_CHECK = 1
